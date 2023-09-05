ffc5a0f348b73731a4f92f38e555aa4a07744a5c
Leonardo Maciel Deschamps <leonardomacieldeschamps@gmail.com>
2023-09-05 20:24:10 -0300

    Fórum com DDD & Clean Architecture - Caso de uso: Buscar pergunta pelo slug

--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -4,6 +4,11 @@ import { Question } from '@/domain/forum/enterprise/entities/question'
 export class InMemoryQuestionsRepository implements QuestionsRepository {
   public items: Question[] = []
 
+  async findBySlug(slug: string) {
+    const question = this.items.find((item) => item.slug.value === slug)
+    return question ?? null
+  }
+
   async create(question: Question) {
     this.items.push(question)
   }
